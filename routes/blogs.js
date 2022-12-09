83f04551ad259f27b536c73f3cb399abea6bc8e1
xavier <omondix214@gmail.com>
2022-12-09 19:51:42 +0300

    connected to mongo DB atlas and created blog schema

--- routes/blogs.js
+++ routes/blogs.js
@@ -1,11 +1,33 @@
+const { request } = require("express");
 const express = require("express");
+const Article = require("../models/article");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.render("blogs/index");
+router.get("/", async (req, res) => {
+  try {
+    const articles = await Article.find({});
+    res.render("blogs/index", { articles: articles });
+  } catch {
+    res.redirect("/");
+  }
 });
 router.get("/new", (req, res) => {
   res.render("blogs/new");
+});
+router.post("/", async (req, res) => {
+  const article = new Article({
+    title: req.body.title,
+    description: req.body.description,
+  });
+  try {
+    await article.save();
+    res.redirect("blogs");
+  } catch {
+    res.render("blogs/new", {
+      article: article,
+      errorMessage: "error creating blog",
+    });
+  }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 
+const bodyParser = require("body-parser");
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
 
@@ -12,8 +14,16 @@ app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to Mongoose"));
 
 app.use("/", indexRouter);
 app.use("/blogs", blogsRouter);
 
-app.listen(5000);
+app.listen(process.env.PORT, () =>
+  console.log(`app runing on port ${process.env.PORT}`)
+);
